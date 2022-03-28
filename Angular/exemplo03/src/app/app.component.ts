e3d7fd41f65b3dc6919c6e655668ac1eb22739d0
Tihuanna <keivn_casapc@hotmail.com>
2022-03-28 16:12:06 -0300

    Ng If, For e Switch

---
+++ Angular/exemplo03/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  
+  //Diretiva NG IF
+  exibirTexto: boolean = false;
+
+  acaoNgIf(){
+    this.exibirTexto = !this.exibirTexto
+  }
+
+  //Diretiva NG FOR
+  cursos = ["Angular", "Java", "C#"];
+
+
+//Diretiva NG Switch
+
+curso_selecionado = "Angular";
+
+
+}
